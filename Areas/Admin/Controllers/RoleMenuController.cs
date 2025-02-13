abb8bfbd209618e0d7e11aa5ebf904bf50c14ccd
Landrex Rebuera <landrex.rebuera@gmail.com>
2025-02-13 10:38:43 +0800

    Initial commit: Added existing .NET project to Git.

---
+++ Areas/Admin/Controllers/RoleMenuController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ACCTNGSYS.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Route("admin/role-menu")]
+public class RoleMenuController : Controller
+{
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ACCTNGSYS.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Route("admin/user")]
+public class UserController : Controller
+{
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
